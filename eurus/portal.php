ce9db7cf0f19292be24c9fbc5f92f419343a3333
Marcel Feller <marcel.feller@study.beds.ac.uk>
2017-01-24 13:22:18 -0800

    Version 1.0

---
+++ eurus/portal.php
@@ -0,0 +1,181 @@
+<?php
+
+/*
+
+bWAPP, or a buggy web application, is a free and open source deliberately insecure web application.
+It helps security enthusiasts, developers and students to discover and to prevent web vulnerabilities.
+bWAPP covers all major known web vulnerabilities, including all risks from the OWASP Top 10 project!
+It is for security-testing and educational purposes only.
+
+Enjoy!
+
+Malik Mesellem
+Twitter: @MME_IT
+
+bWAPP is licensed under a Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License (http://creativecommons.org/licenses/by-nc-nd/4.0/). Copyright © 2014 MME BVBA. All rights reserved.
+
+*/
+
+include("security.php");
+include("security_level_check.php");
+include("selections.php");
+
+if(isset($_POST["form"]) && isset($_POST["bug"]))
+{
+
+    $key = $_POST["bug"];
+    $bug = explode(",", trim($bugs[$key]));
+
+    // Debugging
+    // echo " value: " . $bug[0];
+    // echo " filename: " . $bug[1] . "<br />";
+
+    header("Location: " . $bug[1]);
+
+}
+
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+<!--<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Architects+Daughter">-->
+<link rel="stylesheet" type="text/css" href="stylesheets/stylesheet.css" media="screen" />
+<link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon" />
+
+<!--<script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>-->
+<script src="js/html5.js"></script>
+
+<title>EURUS- Login</title>
+
+</head>
+
+<body>
+
+<header>
+
+<img src="images/eurus.png">
+
+</header>
+
+<div id="menu">
+
+    <table>
+
+        <tr>
+
+            <td><font color="#7ad13f">Bugs</font></td>
+            <td><a href="security_level_set.php">Set Security Level</a></td>
+            <td><a href="logout.php" onclick="return confirm('Are you sure you want to leave?');">Logout</a></td>
+            <td><font color="red">Welcome <?php if(isset($_SESSION["login"])){echo ucwords($_SESSION["login"]);}?></font></td>
+
+        </tr>
+
+    </table>
+
+</div>
+
+<div id="main">
+
+    <h1>Portal</h1>
+
+    <p><i>Which bug do you want to hack today? :)</i></p>
+
+    <form action="<?php echo($_SERVER["SCRIPT_NAME"]);?>" method="POST">
+
+        <select name="bug" size="9" id="select_portal">
+
+            <?php
+
+            // Lists the options from the array 'bugs' (bugs.txt)
+            foreach ($bugs as $key => $value)
+            {
+
+               $bug = explode(",", trim($value));
+
+               // Debugging
+               // echo "key: " . $key;
+               // echo " value: " . $bug[0];
+               // echo " filename: " . $bug[1] . "<br />";
+
+               echo "<option value='$key'>$bug[0]</option>";
+
+            }
+
+            ?>
+
+        </select>
+
+        <br />
+
+        <button type="submit" name="form" value="submit">Hack</button>
+
+    </form>
+
+</div>
+
+
+
+<div id="security_level">
+
+    <form action="<?php echo($_SERVER["SCRIPT_NAME"]);?>" method="POST">
+
+        <label>Set your security level:</label><br />
+
+        <select name="security_level">
+
+            <option value="0">low</option>
+            <option value="1">medium</option>
+            <option value="2">high</option>
+
+        </select>
+
+        <button type="submit" name="form_security_level" value="submit">Set</button>
+        <font size="4">Current: <b><?php echo $security_level?></b></font>
+
+    </form>
+
+</div>
+
+<div id="bug">
+
+    <form action="<?php echo($_SERVER["SCRIPT_NAME"]);?>" method="POST">
+
+        <label>Choose your bug:</label><br />
+
+        <select name="bug">
+
+<?php
+
+// Lists the options from the array 'bugs' (bugs.txt)
+foreach ($bugs as $key => $value)
+{
+
+   $bug = explode(",", trim($value));
+
+   // Debugging
+   // echo "key: " . $key;
+   // echo " value: " . $bug[0];
+   // echo " filename: " . $bug[1] . "<br />";
+
+   echo "<option value='$key'>$bug[0]</option>";
+
+}
+
+?>
+
+
+        </select>
+
+        <button type="submit" name="form_bug" value="submit">Hack</button>
+
+    </form>
+
+</div>
+
+</body>
+
+</html>
---
+++ eurus/xss_custom_header.php
@@ -0,0 +1,192 @@
+<?php
+
+/*
+
+bWAPP, or a buggy web application, is a free and open source deliberately insecure web application.
+It helps security enthusiasts, developers and students to discover and to prevent web vulnerabilities.
+bWAPP covers all major known web vulnerabilities, including all risks from the OWASP Top 10 project!
+It is for security-testing and educational purposes only.
+
+Enjoy!
+
+Malik Mesellem
+Twitter: @MME_IT
+
+bWAPP is licensed under a Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License (http://creativecommons.org/licenses/by-nc-nd/4.0/). Copyright © 2014 MME BVBA. All rights reserved.
+
+*/
+
+include("security.php");
+include("security_level_check.php");
+include("functions_external.php");
+include("selections.php");
+
+function xss($data)
+{
+         
+    switch($_COOKIE["security_level"])
+    {
+        
+        case "0" : 
+            
+            $data = no_check($data);            
+            break;
+        
+        case "1" :
+            
+            $data = xss_check_4($data);
+            break;
+        
+        case "2" :            
+                       
+            $data = xss_check_3($data);            
+            break;
+        
+        default : 
+            
+            $data = no_check($data);            
+            break;  
+
+    }       
+
+    return $data;
+
+}
+
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+<!--<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Architects+Daughter">-->
+<link rel="stylesheet" type="text/css" href="stylesheets/stylesheet.css" media="screen" />
+<link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon" />
+
+<!--<script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>-->
+<script src="js/html5.js"></script>
+
+<title>EURUS - XSS</title>
+
+</head>
+
+<body onload="process()">
+    
+<header>
+
+<img src="images/eurus.png">
+
+</header>   
+
+<div id="menu">
+      
+    <table>
+        
+        <tr>
+            
+            <td><a href="portal.php">Bugs</a></td>
+
+            <td><a href="security_level_set.php">Set Security Level</a></td>
+
+            <td><a href="logout.php" onclick="return confirm('Are you sure you want to leave?');">Logout</a></td>
+            <td><font color="red">Welcome <?php if(isset($_SESSION["login"])){echo ucwords($_SESSION["login"]);}?></font></td>
+            
+        </tr>
+        
+    </table>   
+   
+</div> 
+
+<div id="main">
+
+    <h1>XSS - Reflected (Custom Header)</h1>
+
+    <p>Some web clients use custom HTTP request headers...</p>
+
+    <p>
+
+    Content of our <b>bWAPP</b> header:
+
+<?php
+
+foreach(getallheaders() as $name => $value)
+{
+
+    if($name == "bWAPP")
+    {
+
+        echo "<i>" . xss($value) ."</i>";
+
+    }
+
+}
+
+?>
+    </p>
+
+</div>
+    
+
+    
+<div id="security_level">
+  
+    <form action="<?php echo($_SERVER["SCRIPT_NAME"]);?>" method="POST">
+        
+        <label>Set your security level:</label><br />
+        
+        <select name="security_level">
+            
+            <option value="0">low</option>
+            <option value="1">medium</option>
+            <option value="2">high</option> 
+            
+        </select>
+        
+        <button type="submit" name="form_security_level" value="submit">Set</button>
+        <font size="4">Current: <b><?php echo $security_level?></b></font>
+        
+    </form>   
+    
+</div>
+    
+<div id="bug">
+
+    <form action="<?php echo($_SERVER["SCRIPT_NAME"]);?>" method="POST">
+        
+        <label>Choose your bug:</label><br />
+        
+        <select name="bug">
+   
+<?php
+
+// Lists the options from the array 'bugs' (bugs.txt)
+foreach ($bugs as $key => $value)
+{
+    
+   $bug = explode(",", trim($value));
+   
+   // Debugging
+   // echo "key: " . $key;
+   // echo " value: " . $bug[0];
+   // echo " filename: " . $bug[1] . "<br />";
+   
+   echo "<option value='$key'>$bug[0]</option>";
+ 
+}
+
+?>
+
+
+        </select>
+        
+        <button type="submit" name="form_bug" value="submit">Hack</button>
+        
+    </form>
+    
+</div>
+      
+</body>
+    
+</html>
